407f880a435ed60f94286e8c223ff4b48d71da6b
RutvikRJ6800 <rutvik.rj6800@gmail.com>
2022-04-23 19:29:08 +0530

    remove duplicate elements from sorted array

---
+++ Leetcode/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i,j;
+        j=0;
+        for(i=1; i<nums.size();i++){
+            if(nums[i]!=nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+    
+        return ++j;
+    }
+};
