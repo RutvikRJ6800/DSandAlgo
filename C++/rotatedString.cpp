9649f255c3414c3958e61cff92590471ba679ae7
RutvikRJ6800 <rutvik.rj6800@gmail.com>
2022-05-09 18:02:37 +0530

    check weather sting is rotation of another

---
+++ C++/rotatedString.cpp
@@ -0,0 +1,72 @@
+// Write a Code to check whether one string is a rotation of another
+// 
+#include<bits/stdc++.h>
+using namespace std;
+
+bool rotatedString(string s, string goal){
+// KMP algorithm
+    if(s.size()!=goal.size()){
+        return false;
+    }
+
+    string temp=s+s;
+    if(temp.find(goal)!=string::npos){
+        return true;
+    }
+    return false;
+
+
+
+
+
+// queue approch
+    // if(s.size()!=goal.size()){
+    //     return false;
+    // }
+
+    // queue<char> q,q1;
+    // for(int i=0; i<s.size(); i++){
+    //     q.push(s[i]);
+    //     q1.push(goal[i]);
+    // }
+
+    // int i=0;
+    // while (i<s.size())
+    // {
+    //     if(q==q1){
+    //         return true;
+    //     }
+    //     char c=q.front();
+    //     q.pop();
+    //     q.push(c);
+    //     i++;
+    // }
+
+    // return false;
+
+
+    // substring approach
+    // if(s==goal){
+    //     return true;
+    // }
+
+    // int i=1;
+    // int n=s.size();
+    // while(i<n-1){
+    //     s = s.substr(1) + s[0];
+    //     i++;
+    //     if(s==goal){
+    //         return true;
+    //     }
+    // }
+    // return false;
+
+}
+
+
+int main(){
+
+    // cout<<rotatedString("abcde","cdeab");
+    cout<<rotatedString("abcde","cdebb");
+    return 0;
+}
