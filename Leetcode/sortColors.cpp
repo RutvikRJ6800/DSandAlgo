0302811f0fd8d6e8ae6f68a816fe1d4c95a41c0e
RutvikRJ6800 <rutvik.rj6800@gmail.com>
2022-06-09 10:59:50 +0530

    sort 0,1,2

--- Leetcode/sortColors.cpp
+++ Leetcode/sortColors.cpp
@@ -1,7 +1,50 @@
 // https://leetcode.com/problems/sort-colors/
+// https://www.youtube.com/watch?v=oaVa-9wmpns&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=3
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        // datch national flag algorithm-O(N)(1 pass)
+
+        int low,mid,high;
+        low=0;
+        mid=0;
+        high=nums.size()-1;
+
+        while(mid<high+1){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                high++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+
+
+
+//         counting sort approch - O(N)*2(2 pass) time; O(3) space;
+       //  vector<int> count(3,0);
+       //  for(int i=0; i<nums.size(); i++){
+       //      count[nums[i]]++;
+       //  }
+       //  int j=0,i=0;
+        
+       // while(i<nums.size()){
+       //      if(count[j]==0){
+       //          j++;
+       //          continue;
+       //      }
+       //      nums[i]=j;
+       //      count[j]--;
+       //     i++;
+       //  }
+
+        // O(N2) time O(1) space
         // for(int i=0; i<nums.size();i++){
         //     for(int j=i; j<nums.size(); j++){
         //         if(nums[i]>nums[j]){
@@ -11,22 +54,6 @@ public:
         // }
         
         
-//         counting sort approch - O(N) time; O(3) space;
-        vector<int> count(3,0);
-        for(int i=0; i<nums.size(); i++){
-            count[nums[i]]++;
-        }
-        int j=0,i=0;
-        
-       while(i<nums.size()){
-            if(count[j]==0){
-                j++;
-                continue;
-            }
-            nums[i]=j;
-            count[j]--;
-           i++;
-        }
         
     }
 };
