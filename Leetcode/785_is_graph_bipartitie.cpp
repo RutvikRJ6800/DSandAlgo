25760835b4d2b43ed9a54bb4a03f7330e3493c86
RutvikRJ6800 <rutvik.rj6800@gmail.com>
2022-12-04 22:27:17 +0530

    Added DFS approach to bipartite problem

--- Leetcode/785_is_graph_bipartitie.cpp
+++ Leetcode/785_is_graph_bipartitie.cpp
@@ -33,3 +33,39 @@ public:
         
     }
 };
+
+
+/*
+// using DFS
+class Solution {
+public:
+    bool startDFS(int node, vector<int>&color, vector<vector<int>>& graph){
+        for(auto it: graph[node]){
+            if(color[it] == -1){
+                color[it] = 1 - color[node]; // color with contradict color to node
+                bool res = startDFS(it, color, graph);
+                if(!res) return false; // if found odd length cycle then return false;
+            }
+            else if(color[node] == color[it]) return false;
+        }
+        
+        return true; // no odd length cycle found so return true.
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> color(V, -1);
+        
+        for(int i=0; i<V; i++){
+            if(color[i]==-1){
+                color[i] = 1;
+                bool res = startDFS(i, color, graph);
+                if(!res) return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+*/
