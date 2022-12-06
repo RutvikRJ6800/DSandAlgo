194b9f03f7298d188712483f6181bb7d2d40a6b0
RutvikRJ6800 <rutvik.rj6800@gmail.com>
2022-12-06 17:10:58 +0530

    added rec and memoization approach in unboundedKS

--- GFG/unboundedKnapsack.cpp
+++ GFG/unboundedKnapsack.cpp
@@ -9,6 +9,49 @@ using namespace std;
 
 class Solution{
 public:
+    /* Reccursive Solution
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        if(N==0 || W==0) return 0;
+        
+        if(wt[N-1] <= W){
+            // 2 choice
+            return max(val[N-1] + knapSack(N, W-wt[N-1],val, wt), knapSack(N-1, W , val, wt));
+        }
+        
+        else{
+            return knapSack(N-1, W , val, wt);
+        }
+    }
+    */
+
+   /*
+   Memoization Approach
+    int rec(int N, int W, int val[], int wt[], vector<vector<int>> &dp){
+            if(N==0 || W==0) return 0;
+            
+            if(dp[N][W] != -1) return dp[N][W]; 
+            if(wt[N-1] <= W){
+                // 2 choice
+                dp[N][W] = max(val[N-1] + rec(N, W-wt[N-1],val, wt, dp), rec(N-1, W , val, wt, dp));
+                return dp[N][W];
+            }
+            
+            else{
+                dp[N][W] = rec(N-1, W , val, wt, dp);
+                return dp[N][W];
+            }
+        }
+        
+        int knapSack(int N, int W, int val[], int wt[])
+        {
+            // code here
+            vector<vector<int>> dp(N+1, vector<int> (W+1,-1));
+            return rec(N, W , val, wt, dp);
+            
+        }
+   */
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
